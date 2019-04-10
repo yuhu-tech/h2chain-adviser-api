e97ebadfbec63c5a73479631bd6700d2544be0b3
lutianzhou001 <lutianzhou@yuhu.tech>
2019-04-10 14:51:02 +0800

    add change password function

--- server/src/resolvers/mutation/auth.js
+++ server/src/resolvers/mutation/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const {need,orderbyorderid,orderbydate,order2,order3,user,orderpayload}= require('../mock')
-
+const {getUserId} = require('../../utils')
 
 const auth = {
   async signup(parent, args, ctx, info) {
@@ -40,19 +40,28 @@ const auth = {
   },
 
   async changepassword(parent, args, ctx, info){
-    //   const users = await ctx.prisma.users({where: { email } })
-    //  if (!users){
-    //   throw new Error(`No such user found for email: ${email}`)
-    // }
-    //  const user = users[0]
-
-    //const valid = await bycrypt.compare(password,user.password)
-    // if (!valid) {
-    //   throw new Error ('Invalid Password')
-    // }
-    return{"error":false}
+    const id = getUserId(ctx)
+    console.log(id);
+    console.log(args)
+    const users = await ctx.prisma.users({where:{id}})
+      if (!users){
+       throw new Error(`No such user found for email: ${email}`)
+       }
+      const valid = await bcrypt.compare(args.oldpassword,users[0].password)
+      if (!valid) {
+         throw new Error ('Invalid Password')
+      }
+      else {
+        const newPassword = await bcrypt.hash(args.newpassword, 10)
+        const returning = await ctx.prisma.updateUser(
+          {
+            data:{password: newPassword},
+            where:{id: users[0].id}
+          }
+        )
+      }
+      return{"error":false}
   }
-
 }
 
 
